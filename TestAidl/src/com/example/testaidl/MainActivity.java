4e6dc37ff5da6544722280b4ae5e7004ff93575a
yulihzn <330193303@qq.com>
2014-10-23 17:02:40 +0800

    test

--- TestAidl/src/com/example/testaidl/MainActivity.java
+++ TestAidl/src/com/example/testaidl/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity implements ServiceConnection{
 		startService(service);
 		bindService(service, this, Context.BIND_AUTO_CREATE);
 		/*yulihznyulihznyulihznyulihznyulihznyulihznyulihznyulihzn*/
-		Log.d("yuli", "yuli");
+		Log.d("yuli", "yulihzn");
 	}
 	
 	@Override
